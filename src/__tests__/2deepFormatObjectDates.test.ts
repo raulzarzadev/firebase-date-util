4ad4640bbaa6167b641ece7cbee0615760111f2d
zasara <raulzarza@gmail.com>
2022-06-23 15:02:58 -0600

    0.3.1

--- src/__tests__/2deepFormatObjectDates.test.ts
+++ src/__tests__/2deepFormatObjectDates.test.ts
@@ -2,9 +2,9 @@ import { Timestamp } from 'firebase/firestore';
 import { Dates } from '../Dates';
 import { DEEP_OBJECT } from '../DATES_OBJS_TESTS';
 
-const obj = DEEP_OBJECT.obj
+const obj = DEEP_OBJECT.obj;
 
-const expecting = DEEP_OBJECT.expecting
+const expecting = DEEP_OBJECT.expecting;
 
 test('deep format object dates to number', () => {
   expect(Dates.deepFormatObjectDates(obj, 'number')).toStrictEqual(expecting(Number));
--- src/__tests__/deepFormatArrayDates.test.ts
+++ src/__tests__/deepFormatArrayDates.test.ts
@@ -2,10 +2,10 @@ import { Timestamp } from 'firebase/firestore';
 import { Dates } from '../Dates';
 import { OBJ_ARRAYS } from '../DATES_OBJS_TESTS';
 
-const newTimestampDate = Timestamp.fromDate(new Date())
+const newTimestampDate = Timestamp.fromDate(new Date());
 
-const obj = OBJ_ARRAYS.obj
-const expecting = OBJ_ARRAYS.expecting
+const obj = OBJ_ARRAYS.obj;
+const expecting = OBJ_ARRAYS.expecting;
 
 test('deep  format array dates to number', () => {
   expect(Dates.deepFormatObjectDates(obj, 'number')).toStrictEqual(expecting(Number));
